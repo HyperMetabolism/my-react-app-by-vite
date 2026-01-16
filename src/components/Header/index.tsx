4ece89ddce76efe6a9b0a9e7a6c0367e12eb1415
Eric <2967182759@qq.com>
2026-01-17 02:12:49 +0800

    feat: 基于vite初始化react项目，并且使用在vite.config使用css常用配置

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            this is header
+        </header>
+    )
+}
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, normalizePath } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import autoprefixer from 'autoprefixer'
+
+const variablePath  = normalizePath(path.resolve(__dirname, './src/variable.scss'));
+
+// https://vite.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '${variablePath}'; `
+      }
+    },
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    },
+    // PostCSS配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist:  ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
+    }
+  },
+  root: path.resolve(__dirname, ''),
+  plugins: [react()],
+})
